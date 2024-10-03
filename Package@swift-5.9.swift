292715ffca3b330513ca23d56a0fd47d908f72f5
Jasmin Eilers <4738799+jaeilers@users.noreply.github.com>
2024-10-03 18:43:20 +0200

    Update toolchain to Swift 6

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -29,5 +29,6 @@ let package = Package(
             name: "NSAttributedStringBuilderTests",
             dependencies: ["NSAttributedStringBuilder"]
         )
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
